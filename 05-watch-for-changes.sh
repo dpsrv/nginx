058bc68038cb3b605e2a4753f8044d775fa2dc3e
Max <max@maxf.net>
2025-06-07 19:55:27 +0000

    updated

--- 05-watch-for-changes.sh
+++ 05-watch-for-changes.sh
@@ -2,12 +2,12 @@
 
 cp /etc/nginx/nginx.conf.init /etc/nginx/nginx.conf
 ls -la /etc/nginx/ >&2
-rm -f /etc/nginx/conf.d/*
+rm -f /etc/nginx/conf.d/* || true
 cp -r /etc/nginx/conf.init/* /etc/nginx/conf.d/
 
 lastChange=$(stat -c %y /etc/letsencrypt/live/domain/fullchain.pem || true)
 if [ -n "$lastChange" ]; then
-	for file in /etc/nginx/conf.d/*.conf.ssl; do
+	for file in /etc/nginx/conf.init/*.conf.ssl; do
 		mv $file ${file%.ssl}
 	done
 	sleep=86400
